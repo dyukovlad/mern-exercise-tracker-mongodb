9135820de5ce97a95206cc38d9630880b984b9c5
Vlad Dyukov <dyukovlad@MacBook-Pro-Vlad.local>
2020-01-10 11:57:56 +0300

    edit full arch

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+async function start() {
+  try {
+    await mongoose
+      .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      })
+      .then(console.log("DB Connection Successfull"))
+      .catch(err => {
+        console.error(err);
+      });
+    //run
+    app.listen(PORT, () => console.log(`App started on port ${PORT}`));
+  } catch (error) {
+    console.log("Server error", error.message);
+    process.exit(1);
+  }
+}
+
+start();
